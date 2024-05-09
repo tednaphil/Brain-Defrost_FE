09b357b6b941449efe28c67db3695e15afddfbb8
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-08 20:35:19 -0500

    Change title

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Brain Defrost FrontEnd
         </p>
         <a
           className="App-link"
