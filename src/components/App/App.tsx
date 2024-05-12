980544c83ede6c718af17bd867d0fe69faa8416a
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-11 21:07:36 -0500

    Define and export getGame network request

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
+import { getGame } from '../Util/fetchCalls'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    fetchGame()
+  }, [])
+
+  const fetchGame = async () => {
+    try {
+      const game = await getGame()
+      console.log(game)
+    } catch(error) {
+        console.log(`fetchGame catch block ${error}`)
+    }
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
--- src/components/Util/fetchCalls.tsx
+++ src/components/Util/fetchCalls.tsx
@@ -1,1 +1,17 @@
-export {}
+
+const getGame = async () => {
+    try {
+        const response = await fetch('https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games/1')
+        if(!response.ok) {
+            const status = response.status
+            console.log(status)
+            throw new Error(`Couldn't get game ${status}`)
+        }
+        return await response.json();
+    }
+    catch (error: unknown) {
+        console.log('API CALLS catch block - game', error)
+        throw error
+    }
+}
+export { getGame }
