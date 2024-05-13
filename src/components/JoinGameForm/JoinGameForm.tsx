60be71f607f8a7cd4ffa788f27dce4524d19b379
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-13 13:27:04 -0500

    Add JoinGameForm elements and style rules

--- src/components/JoinGameForm/JoinGameForm.tsx
+++ src/components/JoinGameForm/JoinGameForm.tsx
@@ -1,10 +1,16 @@
 import './JoinGameForm.css';
+// import { useParams } from 'react-router-dom';
 
 function JoinGameForm() {
+    // const { gameid } = useParams();
+    //check if game exists
+    //if it does not, show user feedback and a link to home page
+
     return (
-        <>
-        <h2>Join Game Form</h2>
-        </>
+        <form className='entry-form'>
+                <input id='display-name-input'type='text' placeholder='Enter Display Name' required></input>
+                <button>Join Game!</button>
+        </form>
     )
 }
 
