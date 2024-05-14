f9820d36cf11c0b17cf8f17bd74666063e5d8678
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-14 15:23:06 -0500

    Update test interception stub

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -18,10 +18,20 @@ describe('Brain Defrost User Stories', () => {
     .get('.create-btn').click()
   })
   it('Allows user to join a game', () => {
+    cy.intercept('POST', 'https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games',
+      {
+        statusCode: 201,
+        fixture: 'createdGame'
+      }).as('createGame')
+    cy.get('#name').type('creator')
+    .get('#category').type('music')
+    .get('#players').type('7')
+    .get('#questions').type('8')
+    .get('.create-btn').click()
     cy.intercept('POST', 'https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games/1/players',
       {
         statusCode: 201,
-        fixture: 'createdPlayer1'
+        fixture: 'createdPlayer2'
       }).as('createPlayer')
     cy.visit('http://localhost:3000/Brain-Defrost_FE/join/1')
     .get('#display-name-input').type('student1')
