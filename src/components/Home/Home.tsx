02659b8282c3f68bc76d22f3b85fab0578b36079
Ethan Duvall <eduvall23@turing.edu>
2024-05-13 22:15:30 -0600

    working navigate to new page and save form data

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,22 +1,57 @@
+import React, { FormEvent, useState } from "react";
 import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  let categoryPlaceholder = "History";
+  const [formData, setFormData] = useState({
+    name: "",
+    category: "",
+    players: "",
+    questions: ""
+  });
 
+  const Navigate = useNavigate()
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-  
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+    createGame();
+  };
+
+  function createGame() {
+    console.log("I made a fetchcall!")
+    Navigate(`Game/Lobby/${101213}`)
+  }
+
   return (
     <div className="home">
       <h2 className="form-title">Generate A New Trivia Game!</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <section>
-          <label htmlFor="category"> Select A Category </label>
-          <input type="text" name="category" required />
+          <label htmlFor="name"> Choose a display name</label>
+          <input
+            type="text"
+            className="name-input"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </section>
         <div className="form-holder">
           <section>
-            <p className="category-display">{categoryPlaceholder}</p>
-            <label htmlFor="category-display">Category</label>
+            <input
+              type="text"
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="category"> Select A Category </label>
           </section>
           <section>
             <input
@@ -25,6 +60,8 @@ function Home() {
               min="1"
               max="30"
               placeholder="1"
+              value={formData.players}
+              onChange={handleChange}
               required
             />
             <label htmlFor="players">How many players</label>
@@ -36,6 +73,8 @@ function Home() {
               min="1"
               max="25"
               placeholder="1"
+              value={formData.questions}
+              onChange={handleChange}
               required
             />
             <label htmlFor="questions">How many Questions</label>
