c7cba7522cc1d47d86eb555c902d60f955dd5f5b
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-13 20:32:17 -0500

    Add network interceptions

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -6,7 +6,21 @@ describe('Brain Defrost User Stories', () => {
     cy.get('h1').contains('Brain Defrost')
   })
   it('Allows user to generate a new game', () => {
+    cy.intercept('POST', 'https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games',
+      {
+        statusCode: 201,
+        fixture: 'createdGame'
+      }).as('createGame')
+
   })
   it('Allows user to join a game', () => {
+    cy.intercept('POST', 'https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games/1/players',
+      {
+        statusCode: 201,
+        fixture: 'createdPlayer1'
+      }).as('createPlayer')
+    cy.visit('http://localhost:3000/Brain-Defrost_FE/join/1')
+    .get('#display-name-input').type('student1')
+    .get('#join-game-button').click()
   })
 })
