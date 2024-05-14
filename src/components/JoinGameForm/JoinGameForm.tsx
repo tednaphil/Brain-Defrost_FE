6b756a8295e1d05957bb508ae5d909fa19dbfdc5
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-14 16:08:17 -0500

    Refactor name availability checker

--- src/components/JoinGameForm/JoinGameForm.tsx
+++ src/components/JoinGameForm/JoinGameForm.tsx
@@ -24,38 +24,41 @@ function JoinGameForm({players, setPlayers}: Props) {
         setSessionPlayers(sessionPlayers)
     }, [])
 
-    useEffect(() => {
-        const availability = isNameAvailable();
-        setNameAvailable(availability);
-    }, [displayName])
+    const isNameAvailable = (nameInput: string) => {
+        // @ts-expect-error
+        const playerNames = sessionPlayers.map(player => player.attributes.display_name)
+        console.log('playerNames', playerNames)
+        const matches = playerNames.filter(name => nameInput === name)
+        console.log({matches})
+        if (matches.length) {
+            setNameAvailable(false)
+            return false
+        } else {
+            setNameAvailable(true)
+            return true
+        }
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDisplayName(e.target.value)
+        isNameAvailable(e.target.value)
+    }
     
     //on mount, check if game exists
     //if it does not, show user feedback and a link to home page
     //if it does return the form elemnents 
 
-    //check if displayname is available
-    //if displayname is already in use, display message to user
-    //if displayname is available and game not in progress and the max number of players is not met,
-    //allow user to join
-    const isNameAvailable = () => {
-        // @ts-expect-error
-        const playerNames = sessionPlayers.map(player => player.attributes.display_name)
-        console.log('playerNames', playerNames)
-        const matches = playerNames.filter(name => displayName === name)
-        console.log({matches})
-        if (matches.length) {
-            return false
-        } else {
-            return true
-        }
-    }
+    
 
     const createPlayer = async (gameID: string  | undefined, nameString: string) => {
         const newPlayer = await postPlayer(gameID, nameString);
+        //confirm the displayname is still available
+        //if displayname is already in use at time of submission, display message to user
+        //if displayname is available and game not in progress and the max number of players is not met,
+        //allow user to join etc...
         console.log({newPlayer});
         setPlayers([...players, newPlayer.data]);
         sessionStorage.setItem('players', JSON.stringify([...sessionPlayers, newPlayer.data]));
-        //do we need to also update the game object?
     }
 
     const handleSubmission = async (e: any, gameID: string  | undefined, nameString: string) => {
@@ -67,7 +70,7 @@ function JoinGameForm({players, setPlayers}: Props) {
     return (
         <form className='entry-form' onSubmit={(e) => {handleSubmission(e, gameid, displayName)}}>
                 {!nameAvailable && <p id='display-name-notif'>That display name is taken. Please choose another one!</p>}
-                <input id='display-name-input' name='display name' type='text' placeholder='Enter Display Name' value={displayName} onChange={(e) => setDisplayName(e.target.value)} required></input>
+                <input id='display-name-input' name='display name' type='text' placeholder='Enter Display Name' value={displayName} onChange={(e) => handleChange(e)} required></input>
                 <button id='join-game-button' name='join'>Join Game!</button>
         </form>
     )
