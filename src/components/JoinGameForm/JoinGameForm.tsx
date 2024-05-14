92b0654e58788abf818f5ee1ffe57d8746375610
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-14 15:52:18 -0500

    Add notification for taken username

--- src/components/JoinGameForm/JoinGameForm.tsx
+++ src/components/JoinGameForm/JoinGameForm.tsx
@@ -13,6 +13,7 @@ function JoinGameForm({players, setPlayers}: Props) {
     const { gameid } = useParams<string>();
     const [displayName, setDisplayName] = useState<string>('');
     const [sessionPlayers, setSessionPlayers] = useState([]);
+    const [nameAvailable, setNameAvailable] = useState<boolean>(true);
     
     
     const navigate = useNavigate();
@@ -22,6 +23,11 @@ function JoinGameForm({players, setPlayers}: Props) {
         const sessionPlayers = JSON.parse(sessionStorage.getItem('players'))
         setSessionPlayers(sessionPlayers)
     }, [])
+
+    useEffect(() => {
+        const availability = isNameAvailable();
+        setNameAvailable(availability);
+    }, [displayName])
     
     //on mount, check if game exists
     //if it does not, show user feedback and a link to home page
@@ -31,6 +37,18 @@ function JoinGameForm({players, setPlayers}: Props) {
     //if displayname is already in use, display message to user
     //if displayname is available and game not in progress and the max number of players is not met,
     //allow user to join
+    const isNameAvailable = () => {
+        // @ts-expect-error
+        const playerNames = sessionPlayers.map(player => player.attributes.display_name)
+        console.log('playerNames', playerNames)
+        const matches = playerNames.filter(name => displayName === name)
+        console.log({matches})
+        if (matches.length) {
+            return false
+        } else {
+            return true
+        }
+    }
 
     const createPlayer = async (gameID: string  | undefined, nameString: string) => {
         const newPlayer = await postPlayer(gameID, nameString);
@@ -48,6 +66,7 @@ function JoinGameForm({players, setPlayers}: Props) {
 
     return (
         <form className='entry-form' onSubmit={(e) => {handleSubmission(e, gameid, displayName)}}>
+                {!nameAvailable && <p id='display-name-notif'>That display name is taken. Please choose another one!</p>}
                 <input id='display-name-input' name='display name' type='text' placeholder='Enter Display Name' value={displayName} onChange={(e) => setDisplayName(e.target.value)} required></input>
                 <button id='join-game-button' name='join'>Join Game!</button>
         </form>
