f45f0a8d90022285bc966a6a6459533e3ed9595d
Ethan Duvall <eduvall23@turing.edu>
2024-05-15 08:56:31 -0600

    adjusted form

--- src/components/QuestionForm/QuestionForm.tsx
+++ src/components/QuestionForm/QuestionForm.tsx
@@ -8,45 +8,48 @@ interface Props {
 function QuestionForm({ currentQuestion }: Props) {
   //map through answer options to return answer option inputs to render
   //in the form
+  function nextQuestion(){
+
+  }
   return (
     <>
       <h2 className="question">{currentQuestion.attributes.question_text}</h2>
       <form className="question-form">
         {/* answer options here */}
         <label
-          htmlFor="question-1"
-          className="question-label"
+          htmlFor="option-1"
+          className="option-label"
           style={{ backgroundColor: "#d9534f" }}
         >
-          <input type="radio" name="question-1" className="question-input" />
+          <input type="radio" name="option-1" className="question-input" required/>
           <span>Option</span>
         </label>
         <label
-          htmlFor="question-2"
-          className="question-label"
+          htmlFor="option-2"
+          className="option-label"
           style={{ backgroundColor: "#0275d8" }}
         >
-          <input type="radio" name="question-2" className="question-input" />
+          <input type="radio" name="option-2" className="question-input" required/>
           <span>Option</span>
         </label>
         <label
-          htmlFor="question-3"
-          className="question-label"
+          htmlFor="option-3"
+          className="option-label"
           style={{ backgroundColor: " #5cb85c" }}
         >
-          <input type="radio" name="question-3" className="question-input" />
+          <input type="radio" name="option-3" className="question-input" required/>
           <span>Option</span>
         </label>
         <label
-          htmlFor="question-4"
-          className="question-label"
+          htmlFor="option-4"
+          className="option-label"
           style={{ backgroundColor: "#f0ad4e" }}
         >
-          <input type="radio" name="question-4" className="question-input" />
+          <input type="radio" name="option-4" className="question-input" required/>
           <span>Option</span>
         </label>
 
-        <button className="submit-answer-btn" type="submit">
+        <button onClick={e => nextQuestion()}className="submit-answer-btn" type="submit">
           Submit
         </button>
       </form>
