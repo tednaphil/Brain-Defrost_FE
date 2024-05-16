45ecf2b3eeb063be657a823a7f397c7462add2ca
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-15 21:53:32 -0500

    Add game creation assertions

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -17,7 +17,7 @@ describe('Brain Defrost User Stories', () => {
     .get('.create-btn').contains('Create')
     .get('.footer-title').contains('More info about Brain Defrost')
   })
-  it('Allows user to generate a new game', () => {
+  it('Allows user to generate a new game and see lobby', () => {
     cy.intercept('POST', 'https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games',
       {
         statusCode: 201,
@@ -25,9 +25,19 @@ describe('Brain Defrost User Stories', () => {
       }).as('createGame')
     cy.get('#name').type('creator')
     .get('#topic').type('music')
-    .get('#players').type('7')
-    .get('#questions').type('8')
+    .get('#players').clear().type('7')
+    .get('#questions').clear().type('8')
     .get('.create-btn').click()
+    .url().should('eq', 'http://localhost:3000/Brain-Defrost_FE/game/lobby/1')
+    .get('.game-topic').contains('music')
+    .get('.question-count').contains('8 Questions')
+    .get('.join-url-heading').contains('Share the Link to invite players!')
+    .get('.join-url-container').children().should('have.length', 2)
+    .get('.join-url').contains('http://localhost:3000/Brain-Defrost_FE/join/1')
+    //test copy functionality
+    .get('.start-game-btn').contains('Start Game!')
+    .get('.players-heading').contains('Players')
+    .get('.players').contains('p', 'creator')
   })
   it('Allows user to join a game', () => {
     cy.intercept('POST', 'https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games',
@@ -37,8 +47,8 @@ describe('Brain Defrost User Stories', () => {
       }).as('createGame')
     cy.get('#name').type('creator')
     .get('#topic').type('music')
-    .get('#players').type('7')
-    .get('#questions').type('8')
+    .get('#players').clear().type('7')
+    .get('#questions').clear().type('8')
     .get('.create-btn').click()
     cy.intercept('POST', 'https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games/1/players',
       {
@@ -48,5 +58,6 @@ describe('Brain Defrost User Stories', () => {
     cy.visit('http://localhost:3000/Brain-Defrost_FE/join/1')
     .get('#display-name-input').type('student1')
     .get('#join-game-button').click()
+    //check url and lobby content
   })
 })
