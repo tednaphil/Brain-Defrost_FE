57b2531af768fe795fb1fcdf1d19ed4c40cf2f97
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-15 22:13:24 -0500

    Add start game interception and initial assertions

--- cypress/e2e/GamePlay.spec.cy.ts
+++ cypress/e2e/GamePlay.spec.cy.ts
@@ -1,15 +1,28 @@
 describe('Brain Defrost GamePlay Stories', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/Brain-Defrost_FE')
+    // cy.visit('http://localhost:3000/Brain-Defrost_FE')
   })
   it('Allows user to start a game', () => {
+    cy.intercept('POST', 'https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games',
+      {
+        statusCode: 201,
+        fixture: 'createdGame'
+      }).as('createGame')
+    cy.visit('http://localhost:3000/Brain-Defrost_FE')
+    cy.get('#name').type('creator')
+    .get('#topic').type('music')
+    .get('#players').clear().type('7')
+    .get('#questions').clear().type('8')
+    .get('.create-btn').click()
+    .get('.start-game-btn').click()
+    .get('.question').contains('How many beats is a quarter note in 4/4 time?')
+    .get('.question-form').children().should('have.length', 5)
+    //check timer, question number indicator, option text, and submission button
   })
   it('Allows user to select and submit an answer', () => {
   })
   it('Notifies user if no answer was submitted', () => {
   })
   it('Shows game stats at the end of the game', () => {
-  })
-  it('Allows user to return to home to generate a new game', () => {
   })
 })
