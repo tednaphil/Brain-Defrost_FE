2c17f084b7cfebf9b109e86aa6c30e978124f05c
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-05-16 13:45:13 -0500

    Update CreatedGame fixture

--- cypress/e2e/GamePlay.spec.cy.ts
+++ cypress/e2e/GamePlay.spec.cy.ts
@@ -2,7 +2,7 @@ describe('Brain Defrost GamePlay Stories', () => {
   beforeEach(() => {
     // cy.visit('http://localhost:3000/Brain-Defrost_FE')
   })
-  it('Allows user to start a game', () => {
+  it('Allows user to start and play game', () => {
     cy.intercept('POST', 'https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games',
       {
         statusCode: 201,
@@ -15,14 +15,13 @@ describe('Brain Defrost GamePlay Stories', () => {
     .get('#questions').clear().type('8')
     .get('.create-btn').click()
     .get('.start-game-btn').click()
-    .get('.question').contains('How many beats is a quarter note in 4/4 time?')
+    .get('.question').contains('Who is the lead vocalist of The Beatles?')
     .get('.question-form').children().should('have.length', 5)
     //check timer, question number indicator, option text, and submission button
   })
   it('Allows user to select and submit an answer', () => {
   })
-  it('Notifies user if no answer was submitted', () => {
-  })
+
   it('Shows game stats at the end of the game', () => {
   })
 })
