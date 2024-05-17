a8f27f9eef6d47b7dc4d468b2bbdb2fda074c93b
Ethan Duvall <eduvall23@turing.edu>
2024-05-17 11:54:28 -0600

    Working join link

--- src/components/Lobby/Lobby.tsx
+++ src/components/Lobby/Lobby.tsx
@@ -15,18 +15,28 @@ function Lobby({players}: Props) {
     const location = useLocation();
     const game = location.state;
     const { gameid } = useParams();
-    const joinURL = `http://localhost:3000/Brain-Defrost_FE/join/${gameid}`;
+   // const joinURL = `http://localhost:3000/Brain-Defrost_FE/join/${gameid}`;
     const navigate = useNavigate();
+    const [joinURL, setJoinUrl] = useState('')
 
+    const createJoinLink = () => {
+        console.log(sessionPlayers)
+        console.log(sessionGame)
+        let stringQuestion = JSON.stringify(game)
+        let encodedQuestion = encodeURIComponent(stringQuestion)
+        setJoinUrl(`http://localhost:3000/Brain-Defrost_FE/join/${gameid}/?data=${encodedQuestion}`)
+    }
+    
     useEffect(() => {
         // @ts-expect-error
         const sessionGame = JSON.parse(sessionStorage.getItem('game'))
         setSessionGame(sessionGame)
+        createJoinLink()
         // @ts-expect-error
         const sessionPlayers = JSON.parse(sessionStorage.getItem('players'))
         setSessionPlayers(sessionPlayers)
         console.log('sessionPlayers', sessionPlayers)
-    }, [players])
+    }, [players, createJoinLink])
 
     
     const playerNames = sessionPlayers.map(player => {
@@ -41,7 +51,6 @@ function Lobby({players}: Props) {
     const copyURL = () => {
         navigator.clipboard.writeText(joinURL);
     };
-
 
     const startGame = () => {
         navigate(`/game/play/${gameid}`, {state: sessionGame});
@@ -58,7 +67,7 @@ function Lobby({players}: Props) {
                 <h2 className='question-count lobby-details'>{`${game.attributes.number_of_questions} Questions`}</h2>
                 <h2 className='join-url-heading lobby-details'>Share the Link to invite players!</h2>
                 <div className='join-url-container'>
-                    <p className='join-url'>{joinURL}</p>
+                    <p onClick={copyURL} className='join-url'>Copy Link</p>
                     <button id='copy-url-btn' onClick={copyURL}><Copy /></button>
                 </div>
                 <button className='start-game-btn' onClick={startGame}>Start Game!</button>
