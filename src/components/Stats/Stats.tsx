93b58b23de01500f3fc2ed07919e29b6dafac9f7
Ethan Duvall <eduvall23@turing.edu>
2024-05-17 19:21:47 -0600

    Working stat display just need styling

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -1,11 +1,74 @@
-import './Stats.css';
+import "./Stats.css";
+import { getFinalStats } from "../Util/fetchCalls";
+import { useEffect, useState } from "react";
+import { GetFinalGameStatsResponse, Player } from "../Util/interfaces";
 
 function Stats() {
+  const [finalStats, setFinalStats] = useState<GetFinalGameStatsResponse>();
+  const [rankings, setRankings] = useState<Player[]>([]);
+
+  const fetchStat = async () => {
+    setFinalStats(await getFinalStats());
+  };
+
+  useEffect(() => {
+    fetchStat();
+  }, []);
+
+  useEffect(() => {
+    if (finalStats) {
+      const sortedStats = finalStats.data.relationships.players.data.sort(
+        (a, b) => b.attributes.answers_correct - a.attributes.answers_correct
+      );
+      setRankings(sortedStats);
+    }
+  }, [finalStats]);
+
+  function displayRankings() {
+    return rankings.map((player) => (
+      <li key={player.id}>
+        {player.attributes.display_name} {player.attributes.answers_correct} Pts
+      </li>
+    ));
+  }
+
+  function displayTopThree() {
     return (
-        <>
-        <h2>Stats</h2>
-        </>
-    )
+      <div className="top-three">
+        <div className="second-place">
+          {rankings[1] && <h3>{rankings[1].attributes.display_name}</h3>}
+          <h3>2nd</h3>
+        </div>
+        <div className="first-place">
+          {rankings[0] && <h3>{rankings[0].attributes.display_name}</h3>}
+          <h3>1st</h3>
+        </div>
+        <div className="third-place">
+          {rankings[2] && <h3>{rankings[2].attributes.display_name}</h3>}
+          <h3>3rd</h3>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      <header>
+        <h1>Good game!</h1>
+      </header>
+      <section>
+        <h2>Top Scorers</h2>
+        {displayTopThree()}
+      </section>
+      <section>
+        <h2>Rankings</h2>
+        <ol>{rankings.length > 0 && displayRankings()}</ol>
+      </section>
+      <footer>
+        <button>Generate A New Game</button>
+      </footer>
+    </main>
+  );
 }
 
-export default Stats
+export default Stats;
--- src/components/Util/fetchCalls.tsx
+++ src/components/Util/fetchCalls.tsx
@@ -138,4 +138,30 @@ const getAllPlayers = async () => {
   }
 };
 
-export { getGame, getPlayer, getStats, postPlayer, postGame, patchPlayer, getAllPlayers};
+const getFinalStats = async () => {
+  try {
+    const response = await fetch(
+      `https://c98a077d-6c2a-4ca9-a867-cf11b6279230.mock.pstmn.io/api/v1/games/1/stats`,
+      { method: "GET" }
+    );
+    if (!response.ok) {
+      const status = response.status;
+      console.log(status);
+      throw new Error(`Couldn't get player ${status}`);
+    }
+    return await response.json();
+  } catch (error: unknown) {
+    console.log("API CALLS catch block - patch player", error);
+    throw error;
+  }
+};
+export {
+  getGame,
+  getPlayer,
+  getStats,
+  postPlayer,
+  postGame,
+  patchPlayer,
+  getAllPlayers,
+  getFinalStats
+};
