5e89f7386e80d1c8c482162716c8fd72d0073e58
Ethan Duvall <eduvall23@turing.edu>
2024-05-17 20:07:03 -0600

    Finalized Stats page

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -2,11 +2,12 @@ import "./Stats.css";
 import { getFinalStats } from "../Util/fetchCalls";
 import { useEffect, useState } from "react";
 import { GetFinalGameStatsResponse, Player } from "../Util/interfaces";
+import { useNavigate } from "react-router-dom";
 
 function Stats() {
   const [finalStats, setFinalStats] = useState<GetFinalGameStatsResponse>();
   const [rankings, setRankings] = useState<Player[]>([]);
-
+  const Navigate = useNavigate();
   const fetchStat = async () => {
     setFinalStats(await getFinalStats());
   };
@@ -23,11 +24,13 @@ function Stats() {
       setRankings(sortedStats);
     }
   }, [finalStats]);
-
+  function goHome() {
+    Navigate("/");
+  }
   function displayRankings() {
     return rankings.map((player) => (
       <li key={player.id}>
-        {player.attributes.display_name} {player.attributes.answers_correct} Pts
+        {player.attributes.display_name} {player.attributes.answers_correct}Pts
       </li>
     ));
   }
@@ -36,38 +39,48 @@ function Stats() {
     return (
       <div className="top-three">
         <div className="second-place">
-          {rankings[1] && <h3>{rankings[1].attributes.display_name}</h3>}
-          <h3>2nd</h3>
+          {rankings[1] && (
+            <h3 className="podium-holders">
+              {rankings[1].attributes.display_name}
+            </h3>
+          )}
+          <h3 className="second">2nd</h3>
         </div>
         <div className="first-place">
-          {rankings[0] && <h3>{rankings[0].attributes.display_name}</h3>}
-          <h3>1st</h3>
+          {rankings[0] && (
+            <h3 className="podium-holders">
+              {rankings[0].attributes.display_name}
+            </h3>
+          )}
+          <h3 className="first">1st</h3>
         </div>
         <div className="third-place">
-          {rankings[2] && <h3>{rankings[2].attributes.display_name}</h3>}
-          <h3>3rd</h3>
+          {rankings[2] && (
+            <h3 className="podium-holders">
+              {rankings[2].attributes.display_name}
+            </h3>
+          )}
+          <h3 className="third">3rd</h3>
         </div>
       </div>
     );
   }
 
   return (
-    <main>
-      <header>
-        <h1>Good game!</h1>
-      </header>
-      <section>
+    <div className="stats-display">
+      <h1>Good game!</h1>
+      <section className="podium">
         <h2>Top Scorers</h2>
         {displayTopThree()}
       </section>
-      <section>
+      <section className="rankings-display">
         <h2>Rankings</h2>
-        <ol>{rankings.length > 0 && displayRankings()}</ol>
+        <ol className="rankings-list">
+          {rankings.length > 0 && displayRankings()}
+        </ol>
       </section>
-      <footer>
-        <button>Generate A New Game</button>
-      </footer>
-    </main>
+      <button onClick={goHome}>Generate A New Game</button>
+    </div>
   );
 }
 
